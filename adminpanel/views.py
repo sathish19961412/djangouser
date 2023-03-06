8f22b68cb29772592494860bc8787d79fb75cd77
sathish19961412 <sathishfreelanc5@gmail.com>
2023-03-06 18:00:45 +0530

    Create To Register Form With validation of the User register

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import CreateUserForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
-def home(reqest):
-    return render(reqest,"home.html")
+def home(request):
+    return render(request,"home.html")
 
-def login(reqest):
-    return render(reqest,"login.html")
+def login(request):
+    return render(request,"login.html")
 
-def register(reqest):
-    return render(reqest,"register.html")
+def register(request):
+   if request.method=='POST':
+       name=request.POST['username']
+       email=request.POST['email']
+       password1=request.POST['password1']
+       password2=request.POST['password2']
+
+       if password1==password2:
+            user=User.objects.create_user(username=name,email=email,password=password1)
+            user.save()
+            messages.success(request,'Your account has been Created')
+            return redirect('Login')
+       else:
+            messages.warning(request,'Password MissMatching !!!')
+            return redirect('Register')
+   else:    
+        form=CreateUserForm()
+        return render(request,"register.html",{'form':form})
 
--- adminproject/settings.py
+++ adminproject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'adminpanel',
+    'crispy_forms',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[
+    BASE_DIR / "static",
+]
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
